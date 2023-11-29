164a734ede450829d34866aa2cbb3ebb0d87acc5
Rıdvan SÖYLER <r.soyler@fabay.com.tr>
2023-11-29 18:46:10 +0300

    post metodu eklendi.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using MetaApi.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -115,28 +116,14 @@ app.MapPost("/token",
 
 app.MapGet("/api/customer/status/{status}", [Authorize] async (MetaContext db, int status) => await db.CombinedTables.Where(a => a.Status == (status == 0 ? false : true)).ToListAsync());
 app.MapGet("/api/customer/{id}", [Authorize] async ( MetaContext db, double id) => await db.CombinedTables.Where(a => a.DataId == id).ToListAsync());
-//app.MapPost("/api/marketing/setStatus", [Authorize] async (MetaContext db, HttpContext context, List<double> idList) =>
-//{
-//    if (context.Request.HasJsonContentType())
-//    {
-//        var todo = await context.Request.ReadFromJsonAsync<List<double>>();
-//        if (todo is not null)
-//        {
-//            todo.Name = todo.NameField;
-//        }
-//        return Results.Ok(todo);
-//    }
-//    else
-//    {
-//        return Results.BadRequest();
-//    }
-
-//    await db.CombinedTables.Where(a => idList.Exists(b => a.DataId == b)).ForEachAsync((item) =>
-//        {
-//            item.Status = !item.Status;
-//        });
-//    await db.SaveChangesAsync();
-//});
+app.MapPost("/api/marketing/setStatus", async (MetaContext db, HttpContext context, [FromBody]List<double> idList) =>
+{
+    await db.CombinedTables.Where(a => idList.Exists(b => a.DataId == b)).ForEachAsync((item) =>
+        {
+            item.Status = !item.Status;
+        });
+    await db.SaveChangesAsync();
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
