d618f34dd7b92b6afa2fca23b779f1ef142764d3
Rıdvan SÖYLER <r.soyler@fabay.com.tr>
2023-12-24 17:32:36 +0300

    - record date eklendi.

--- Models/MetaContext.cs
+++ Models/MetaContext.cs
@@ -48,6 +48,7 @@ public partial class MetaContext : DbContext
             entity.Property(e => e.EvBakmaAmaci).HasColumnName("ev_bakma_amaci");
             entity.Property(e => e.KategoriAdi).HasColumnName("kategori_adi");
             entity.Property(e => e.KategoriId).HasColumnName("kategori_id");
+            entity.Property(e => e.RecordDate).HasColumnName("record_date");
         });
 
         OnModelCreatingPartial(modelBuilder);
