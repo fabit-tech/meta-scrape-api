6debe16fc6b8834db8fe588c42e8b31d796a422b
sndogan <149052899+sndogan@users.noreply.github.com>
2024-06-14 09:28:30 +0300

    Update MailCrmFinal.cs

--- Models/MailCrmFinal.cs
+++ Models/MailCrmFinal.cs
@@ -8,40 +8,42 @@ public partial class MailCrmFinal
 {
     public string? Gonderen { get; set; }
 
-    public string? Email { get; set; }
+public string? Email { get; set; }
 
-    public double? Telefon { get; set; }
+public string? Telefon { get; set; }
 
-    public string? Mesaj { get; set; }
+public string? Mesaj { get; set; }
 
-    public string? ReferansUrl { get; set; }
+public string? ReferansUrl { get; set; }
 
-    public string? Kvkk { get; set; }
+public string? Kvkk { get; set; }
 
-    public string? Platform { get; set; }
+public string? Platform { get; set; }
 
-    public string? Dil { get; set; }
+public string? Dil { get; set; }
 
-    public string? Ulke { get; set; }
+public string? Ulke { get; set; }
 
-    public DateTime? CreateDate { get; set; }
+public DateTime? CreateDate { get; set; }
 
-    public double? IsSpam { get; set; }
+public double? IsSpam { get; set; }
 
-    public double? IsDomestic { get; set; }
+public double? IsDomestic { get; set; }
 
-    public string? Projects { get; set; }
-    [Key]
-    public double EmailId { get; set; }
+public string? Projects { get; set; }
 
-    public string? UtmSource { get; set; }
+public int? EmailId { get; set; }
 
-    public string? UtmMedium { get; set; }
+public string? UtmSource { get; set; }
 
-    public string? UtmCampaign { get; set; }
+public string? UtmMedium { get; set; }
 
-    public string? VmSource { get; set; }
+public string? UtmCampaign { get; set; }
 
-    public bool Status { get; set; }
+public string? VmSource { get; set; }
+
+public bool Status { get; set; }
+
+//public DateTime? RecordDate { get; set; }
 
 }
