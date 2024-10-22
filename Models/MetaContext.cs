72f03a04239745d144789d948fa11bd1376fcfae
Nazlı DOĞAN <n.tasdan@fabay.com.tr>
2024-10-22 12:27:52 +0300

    ihale servisleri

--- Models/MetaContext.cs
+++ Models/MetaContext.cs
@@ -14,8 +14,8 @@ public partial class MetaContext : DbContext
     public virtual DbSet<CombinedTable> CombinedTables { get; set; }
     public virtual DbSet<CreateAddSetId> CreateAddSetId { get; set; }
     public virtual DbSet<MailCrmFinal> MailCrmFinal { get; set; }
-    public virtual DbSet<Ihale> Ihale { get; set; }
-    public virtual DbSet<IhaleBirimFiyat> IhaleBirimFiyat { get; set; }
+    //public virtual DbSet<Ihale> Ihale { get; set; }
+    //public virtual DbSet<IhaleBirimFiyat> IhaleBirimFiyat { get; set; }
 
 
 
