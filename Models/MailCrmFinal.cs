aeab0c1275e7dfb1d2d5781ab4b3522b96c9b263
sndogan <149052899+sndogan@users.noreply.github.com>
2025-03-12 12:10:39 +0300

    Update MailCrmFinal.cs

--- Models/MailCrmFinal.cs
+++ Models/MailCrmFinal.cs
@@ -12,35 +12,35 @@ public string? Email { get; set; }
 
 public string? Telefon { get; set; }
 
-public string? Mesaj { get; set; }
+//public string? Mesaj { get; set; }
 
-public string? ReferansUrl { get; set; }
+//public string? ReferansUrl { get; set; }
 
-public string? Kvkk { get; set; }
+//public string? Kvkk { get; set; }
 
-public string? Platform { get; set; }
+//public string? Platform { get; set; }
 
-public string? Dil { get; set; }
+//public string? Dil { get; set; }
 
-public string? Ulke { get; set; }
+//public string? Ulke { get; set; }
 
-public DateTime? CreateDate { get; set; }
+//public DateTime? CreateDate { get; set; }
 
-public double? IsSpam { get; set; }
+//public double? IsSpam { get; set; }
 
-public double? IsDomestic { get; set; }
+//public double? IsDomestic { get; set; }
 
-public string? Projects { get; set; }
+//public string? Projects { get; set; }
 [Key]
 public int EmailId { get; set; }
 
-public string? UtmSource { get; set; }
+//public string? UtmSource { get; set; }
 
-public string? UtmMedium { get; set; }
+//public string? UtmMedium { get; set; }
 
-public string? UtmCampaign { get; set; }
+//public string? UtmCampaign { get; set; }
 
-public string? VmSource { get; set; }
+//public string? VmSource { get; set; }
 
 public bool Status { get; set; }
 
